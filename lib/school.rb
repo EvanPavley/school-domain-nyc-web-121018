b11cf11625f65364cdb3965e0e207da7a8aab92e
Evan Pavley <epavley@sva.edu>
2018-12-11 21:26:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  attr_accessor :school, :roster
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    :roster[grade] unless grade == grade
+    :roster[grade] << name
+  end
+  
+end
